33f06f27d956843c0df11e8709de911d36b932dd
ShamikRoy <shamikroy007@gmail.com>
2020-11-29 18:22:10 -0500

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -38,5 +38,34 @@ def Read_Files():
     stem2words: type: dictionary; key: stem; value: corresponding set of words
     '''
 
+    with open("graph_info.pkl", "rb") as in_file:
+        [graph, id2name, name2id, doc_start, doc_end, \
+         bigram_start, bigram_end, trigram_start, trigram_end, \
+         subframe_start, subframe_end] = pickle.load(in_file)
+
+    '''
+    This file contains pre-processed format of the dataset suitable for the embedding learning.
+    Every element - paragraph (doc), subframe, bigram, trigram; are encoded with a unique numeric id.
+    The mapping to the encoded id and the original name of the element can be found in 'id2name' and 'name2id' dictionaries.
+
+    Then all of the elements are encoded to a graph structure. Where there exists an edge from a paragraph (doc) to a bi/tri-gram
+    if the bi/tri-gram is present in the paragraph, and a bi/tri-gram is connected to its corresponding subframe label (if there is a label).
+    This adjacency list can be found in the 'graph'
+
+    doc_start, doc_end = starting and ending ids of the paragraphs
+    bigram_srart, bigram_end = starting and ending ids of the bigrams
+    trigram_srart, trigram_end = starting and ending ids of the trigrams
+    subframe_srart, subframe_end = starting and ending ids of the subframes
+    '''
+
+    with open("tokenized_paragraphs.pkl", "rb") as f:
+        [weights_matrix, segment2tokenized_text] = pickle.load(f)
+
+    '''
+    This file contains preprocessed data-structures containing segments (paragraphs) in a tokenized format (segment2tokenized_text)
+    weight_matrix is a n*m dimensional matrix, where n=number of unique tokens in the whole corpus and m=GloVe embedding dimension
+    '''
+
+
 if __name__ == '__main__':
     Read_Files()
